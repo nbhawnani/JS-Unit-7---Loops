3e3b17e86dcb109e8a3996ab95a1e8857a3ba696
Nisha Brijesh <92055350+nbhawnani@users.noreply.github.com>
2022-04-26 13:43:09 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,61 @@
+/*
+for (let step = 0; step < 5; step++) {
+// Runs 5 times, with values of step 0 through 4.
+console.log('Walking east one step', step);
+}
+
+
+console.log('Out of the loop');
+*/
+
+/*
+
+const array1 = [1,2,3,4,5]
+// with for loop
+for(let i=0; i<array1.length; i++){
+  
+console.log(array1[i] + 1)
+  
+}
+// Output:
+2,3,4,5,6
+
+// break command comes out of the loop execution:
+*/
+
+/*
+// For each - accesses each value/element of the array
+const nums = [1,2,3,4,5]
+// with forEach loop
+nums.forEach(item => {
+console.log("Item:",item )
+})
+// Output:
+2,3,4,5,6
+
+*/
+
+// For Each - value, index, array itself 
+const numbers = [45, 4, 9, 16, 25];
+
+let txt = "";
+numbers.forEach(myFunction);
+
+function myFunction(value, index, array) {
+  console.log( "Value at index position:",index,"is:",value + " of the array:",array,"\n");
+  
+}
+
+/*
+// For in : gives you the index positions 
+const numbers = [45, 4, 9, 16, 25];
+
+let txt = "";
+for (let x in numbers) {
+  txt += numbers[x]+">>";
+}
+console.log(txt)
+*/
+
+
+
